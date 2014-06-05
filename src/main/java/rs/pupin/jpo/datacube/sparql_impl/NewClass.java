d49a26fee34aae375a9f3b4554aa5705dfccb923
Vuk <vukm@vukm-N53Jn>
2014-06-06 00:02:35 +0200

    test

---
+++ src/main/java/rs/pupin/jpo/datacube/sparql_impl/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rs.pupin.jpo.datacube.sparql_impl;
+
+/**
+ *
+ * @author vukm
+ */
+public class NewClass {
+    
+}
