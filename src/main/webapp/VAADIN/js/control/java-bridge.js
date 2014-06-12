72495346ed7788ef24c756daccdeb0bf643ded52
vukm <vukm@vukm-N53Jn>
2014-06-13 00:06:07 +0200

    manually filled appropriate var in order for the legend to work, obviously hasCodeToObservationValues has map needs to be filled, it is probably empty because cbFuncYearIncentive is never called and even if it is it fails because combo boxes for year and incentive are missing

--- src/main/webapp/VAADIN/js/control/java-bridge.js
+++ src/main/webapp/VAADIN/js/control/java-bridge.js
@@ -27,6 +27,22 @@ function javaSetGraphAndDataSet(graph,ds){
     javaDataSet = ds;
     sessionStorage.setItem('endpoint','http://jpo.imp.bg.ac.rs/sparql');
     sessionStorage.setItem('graph',javaGraph);
+    for (var i=1; i<5; i++) {
+        for (var j=1; j<5; j++) {
+            for (var k=1; k<5; k++){
+                var code = 'RS' + i.toString() + j.toString() + k.toString();
+                var val = i*j*k;
+                hashCodeToObservationValues[code] = val.toString();
+            }
+            var code = 'RS' + i.toString() + j.toString();
+            var val = i*j;
+            hashCodeToObservationValues[code] = val.toString();
+        }
+        var code = 'RS' + i.toString();
+        var val = i;
+        hashCodeToObservationValues[code] = val.toString();
+    }
+//    refreshMap();
 //    sessionStorage.setItem('analysistype','barspace');
 }
 
